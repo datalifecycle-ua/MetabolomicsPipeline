55a11e824b1d5fbc77b7bd9a6265383f0726df5a
JoelParkerUofA <joelparker@arizona.edu>
2023-10-02 17:04:25 -0700

    adding roxygen comments

--- R/create_heatmap_Data.R
+++ R/create_heatmap_Data.R
@@ -1,3 +1,22 @@
+#' Create metadata and matricies for metabolite heatmaps
+#' 
+#' This function creates the required matrices for the metabolite heatmaps. 
+#' 
+#' @param .analysis_data The analysis data containing all of the metabolite and 
+#' metadata information. 
+#' 
+#' @param heatmap_variables A vector of variable names that are NOT metabolites.
+#' 
+#' @param ... Additional arguments that can be passed into the arrange function. 
+#' This parameter will order the columns of the heatmap data. 
+#' 
+#' @return A list of matricies including the heatmap variable (meta data for heatmap)
+#' and the values for the heatmap.
+#' 
+#' 
+
+
+
 # This function creates heatmap data
 create_heatmap_Data <- function(.analysis_data,heatmap_variables,...){
   
