427f6e15beedbf2b77e2c3247a1095b93df492c4
joelparkeruofa <joelparker@joels-air.mynetworksettings.com>
2025-04-25 12:34:24 -0700

    adding data docs

---
+++ R/demoChemAnno.R
@@ -0,0 +1,8 @@
+#' Demo Chemical Annotation
+#'
+#' A small example of chemical annotation data for the MetabolomicsPipeline package.
+#'
+#' @name demoChemAnno
+#' @format A data frame with annotation for 1102 metabolites
+#' @source Generated for demonstration
+"demoChemAnno"
